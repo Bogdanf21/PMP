d9b153143516f8ef1e00dac43bb87d11bdafc8a9
Bogdanf21 <79158552+Bogdanf21@users.noreply.github.com>
2022-10-14 09:37:57 +0300

    added examples

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -24,27 +24,21 @@ def problem1():
 
 # problem 2 ######################################################################
 def problem2():
-    latency = stats.expon.rvs(0, 1 / 4, size=10000)
+
     x = []
     for i in range(0, 10000):
-        match np.random.choice(a=[1, 2, 3, 4], p=[0.25, 0.25, 0.3, 0.2], size=1)[0]:
-            case 1:
-                x.append(stats.gamma.rvs(4, scale=1 / 3, size=1)[0])
-            case 2:
-                x.append(stats.gamma.rvs(4, scale=1 / 2, size=1)[0])
-            case 3:
-                x.append(stats.gamma.rvs(5, scale=1 / 2, size=1)[0])
-            case 4:
-                x.append(stats.gamma.rvs(5, scale=1 / 3, size=1)[0])
-    x += latency
-    favorable = 0
-    for i in x:
-        if i > 3:
-            favorable += 1
-
-    print("Probability: ", favorable / 10000)
-
-    az.plot_density({"Avg:": x})
+        latency = stats.expon.rvs(0, 1 / 4, size=1)[0]
+        choice = np.random.choice(a=[1, 2, 3, 4], p=[0.25, 0.25, 0.3, 0.2], size=1)[0]
+        if choice == 1:
+            x.append(stats.gamma.rvs(4, scale=1 / 3, size=1)[0] + latency)
+        elif choice == 2:
+            x.append(stats.gamma.rvs(4, scale=1 / 2, size=1)[0] + latency)
+        elif choice == 3:
+            x.append(stats.gamma.rvs(5, scale=1 / 2, size=1)[0] + latency)
+        elif choice == 4:
+            x.append(stats.gamma.rvs(5, scale=1 / 3, size=1)[0] + latency)
+    print("Probability: ", (np.asarray(x) > 3).sum() / 10000)
+    az.plot_density({"Density:": x})
     plt.show()
 
 
@@ -54,30 +48,27 @@ def problem3():
     bs = []
     bb = []
     for i in range(0, 100):
-        experiment = []
-        for i in range(0, 10):
-            toss1 = np.random.choice(a=['s', 'b'], p=[0.5, 0.5], size=1)[0]
-            toss2 = np.random.choice(a=['s', 'b'], p=[0.3, 0.7], size=1)[0]
+        exp = []
+        for j in range(0, 10):
+            toss1 = np.random.choice(a=[0, 1], p=[0.5, 0.5], size=1)[0]
+            toss2 = np.random.choice(a=[0, 1], p=[0.3, 0.7], size=1)[0]
             match toss1, toss2:
-                case 's', 's':
-                    experiment.append('ss')
-                case 's', 'b':
-                    experiment.append('sb')
-                case 'b', 's':
-                    experiment.append('bs')
-                case 'b', 'b':
-                    experiment.append('bb')
-        ss.append(experiment.count('ss'))
-        sb.append(experiment.count('sb'))
-        bs.append(experiment.count('bs'))
-        bb.append(experiment.count('bb'))
-    az.plot_posterior({"Stema, Stema": np.array(ss)})
-    az.plot_posterior({"stema, ban": np.array(sb)})
-    az.plot_posterior({"ban, stema" : np.array(bs)})
-    az.plot_posterior({"ban, ban" : np.array(bb)})
+                case 0, 0:
+                    exp.append(1)
+                case 0, 1:
+                    exp.append(2)
+                case 1, 0:
+                    exp.append(3)
+                case 1, 1:
+                    exp.append(4)
+        ss.append(exp.count(1))
+        sb.append(exp.count(2))
+        bs.append(exp.count(3))
+        bb.append(exp.count(4))
+    az.plot_posterior({"ss": np.array(ss), "sb": np.array(sb), "bs" : np.array(bs), "bb" : np.array(bb)})
     plt.show()
 
 
-problem1()
-# problem2()
-#problem3()
+#problem1()
+#problem2()
+problem3()
