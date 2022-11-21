8261a90cf2d7c45465cd1219c0be6f6b43d95c8c
Bogdanf21 <79158552+Bogdanf21@users.noreply.github.com>
2022-11-21 10:52:53 +0200

    lab7 finished

--- lab7/lab7.py
+++ lab7/lab7.py
@@ -8,11 +8,11 @@ from scipy import stats
 
 if __name__ == '__main__':
     data = pd.read_csv("./Prices.csv")
-    Price = data['Price'].values
-    Speed = data['Speed'].values
+    price = data['Price'].values
+    speed = data['Speed'].values
     hard_drive = data['HardDrive'].values
-    Ram = data['Ram'].values
-    Premium = data['Premium'].values
+    ram = data['Ram'].values
+    premium = data['Premium'].values
     the_model = pm.Model()
     # ex1
     with the_model:
@@ -20,10 +20,43 @@ if __name__ == '__main__':
         beta1 = pm.Normal('beta1', mu=0, sd=10)
         beta2 = pm.Normal('beta2', mu=0, sd=10)
         sigma = pm.HalfNormal('sigma', sd=10)
-        mu = pm.Deterministic('mu', alpha + beta1 * Speed + beta2 * np.log(hard_drive))
-        Price_obs = pm.Normal('Price_obs', mu=mu, sd=sigma, observed=Price)
-        trace = pm.sample(1000, tune=1000, cores=4, return_inferencedata=True)
+        mu = pm.Deterministic('mu', alpha + beta1 * speed + beta2 * np.log(hard_drive))
+        Price_obs = pm.Normal('Price_obs', mu=mu, sd=sigma, observed=price)
+        trace = pm.sample(1000, tune=200, chains=1)
 
-    list = trace['beta1'].tolist()
-    az.plot_posterior({"beta1": trace['beta1'].tolist(), "beta2": trace['beta2'].tolist()}, hdi_prob=0.95)
+    dictionary = {
+        'alpha': trace['alpha'].tolist(),
+        'beta1': trace['beta1'].tolist(),
+        'beta2': trace['beta2'].tolist(),
+        'sigma': trace['sigma'].tolist(),
+    }
+
+    df = pd.DataFrame(dictionary)
+    az.plot_posterior(
+        {"alpha": trace['alpha'], "beta1": trace['beta1'], "beta2": trace['beta2'], "sigma": trace['sigma']},
+        hdi_prob=0.95)
     plt.show()
+
+
+    def print_plot():
+        fig, axes = plt.subplots(2, 2, sharex=False, figsize=(10, 8))
+        axes[0, 0].scatter(speed, price, alpha=0.6)
+        axes[0, 1].scatter(hard_drive, price, alpha=0.6)
+        axes[1, 0].scatter(ram, price, alpha=0.6)
+        axes[1, 1].scatter(premium, price, alpha=0.6)
+        axes[0, 0].set_ylabel("Price")
+        axes[0, 0].set_xlabel("Speed")
+        axes[0, 1].set_xlabel("HardDrive")
+        axes[1, 0].set_xlabel("Ram")
+        axes[1, 1].set_xlabel("Premium")
+        plt.savefig('price_correlations.png')
+
+
+    print_plot()
+
+# 3. The processor has an influence on the price of the machine
+
+
+# Bonus: The price of a PC is not influenced by the fact that a manufacturer is premium. From fig 4 in which the pc's
+# are classified by price and whether are premium or not we can observe that both cases follow a distribution in the
+# same manner
